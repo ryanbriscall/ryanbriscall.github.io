36740b323ed3803f99e76aa346c85a52941e13ed
ryanbriscall <ryanbriscall@gmail.com>
2020-06-10 20:12:45 -0600

    add blog post: I-Released-a-Password-Generator-Script-Today

--- blog/posts.js
+++ blog/posts.js
@@ -1,5 +1,6 @@
 /* Create your new content/*.md file, then add it (to the top) here: */
 var posts = [
+    '2020-06-10-I-Released-a-Password-Generator-Script-Today',
     '2020-06-09-I-Released-a-Virtual-Keyboard-Script-Today',
     '2020-06-08-I-Released-a-Static-Blog-System-Today',
     // '2011-03-12-SimpleBackpack-PHP-Fix',
